ecc4d45fa53534959d51a7302f9ae858a477377b
Awemick <lemickjac@gmail.com>
2025-11-28 18:37:10 +0300

    fix: Revert APK download link to old URL

--- src/components/common/MobileAppPromotion.tsx
+++ src/components/common/MobileAppPromotion.tsx
@@ -7,7 +7,7 @@ const MobileAppPromotion: React.FC = () => {
   const [qrCodeDataUrl, setQrCodeDataUrl] = useState<string>('');
 
   // Direct APK download URL from GitHub releases
-  const apkDownloadUrl = 'https://github.com/Awemick/afya_tracker_web_app/releases/download/vr.1.0/Afya.Tracker.apk';
+  const apkDownloadUrl = 'https://github.com/Awemick/afya_tracker_web_app/releases/download/vr.1.0/Afya.Tracker.1.0.apk';
 
   useEffect(() => {
     // Generate QR code for APK download
